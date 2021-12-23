4ed4929270b53d7f9c9c5ef225b31f3e74553f28
shiva raj badu <shivabadu5@gmail.com>
2021-12-23 17:37:42 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import "./App.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const accessKey = "QQbGFfWPjW9cerI5hlDvAErNitxTl7q1syqim_K7WdY";
+  const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://api.unsplash.com/search/photos?page=1&per_page=20&query=${query}&client_id=${accessKey}`
+      )
+      .then((resp) => {
+        setData(resp.data.results);
+      });
+  };
+  const handleClick = (url) => {
+    navigate(`${url}`);
+  };
   return (
     <div className="container">
       <header>
         <h1 className="title">React Image Search</h1>
       </header>
-      <form className="form" action="">
+      <form onSubmit={handleSubmit} className="form" action="">
         <label htmlFor="text"></label>
-        <input placeholder="Search Image ..." id="text" type="text" />
+        <input
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search Image ..."
+          id="text"
+          type="text"
+        />
         <button className="button" type="submit">
           Search
         </button>
@@ -16,30 +43,21 @@ function App() {
       <div className="image-section">
         <h3 className="result">Result:</h3>
         <div className="images">
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
-          <div className="image">
-            <img src="https://picsum.photos/200" alt="" />
-            <h3 className="title">Alone walker</h3>
-          </div>
+          {data.map((photo) => (
+            <div key={photo.id} className="image">
+              <img
+                onClick={() => {
+                  handleClick(photo.urls.full);
+                }}
+                src={photo.urls.thumb}
+                alt=""
+              />
+              <h3 className="title">
+                user:{" "}
+                <a href={photo.user.portfolio_url}>{photo.user.first_name}</a>
+              </h3>
+            </div>
+          ))}
         </div>
       </div>
     </div>
